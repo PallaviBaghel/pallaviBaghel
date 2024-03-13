5d4a49e325a6345ba4124705489a2c41e8c457db
PallaviBaghel <163231751+PallaviBaghel@users.noreply.github.com>
2024-03-13 19:33:01 +0530

    loop
    loop

---
+++ loop1.js
@@ -0,0 +1,9 @@
+const input = require("readline-sync");
+let n=input.question("enter the number: ");
+let c=input.question("enter the name: ")
+let i=1;
+while(i<=n)
+{
+    console.log(c);
+    i++;
+}
