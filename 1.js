d3d97d17124b6dee46a3ebae18137d6415c6869b
PallaviBaghel <163231751+PallaviBaghel@users.noreply.github.com>
2024-03-13 10:16:13 +0530

    greater number
    greater number

---
+++ 1.js
@@ -0,0 +1,10 @@
+const input = require("readline-sync");
+let a=input.questionInt("enter the num: ");
+let b=input.questionInt("enter the num: ");
+if(a>b)
+{
+    console.log(a);
+}
+else{
+    console.log(b);
+}
