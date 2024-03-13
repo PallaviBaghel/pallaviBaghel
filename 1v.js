640d1fde169e60f504b9df11160171f04df7f840
PallaviBaghel <163231751+PallaviBaghel@users.noreply.github.com>
2024-03-13 09:57:54 +0530

    DOB
    DOB

---
+++ 1v.js
@@ -0,0 +1,8 @@
+const input = require("readline-sync");
+let d=input.questionInt("enter the date: ");
+let m=input.question("enter the month: ");
+let y=input.questionInt("enter the year: ");
+
+console.log(d);
+console.log(m);
+console.log(y);
