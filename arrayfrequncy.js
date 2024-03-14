c7be81089e709b3242b3bc07e4f84df66a6d1caf
PallaviBaghel <163231751+PallaviBaghel@users.noreply.github.com>
2024-03-14 18:53:57 +0530

    array
    array

---
+++ arrayfrequncy.js
@@ -0,0 +1,35 @@
+const input = require("readline-sync");
+let n=input.question("enter the number: ");
+let a=[]
+let i=0;
+while(i<n)
+{
+    a[i]=input.questionInt("enter the num: ");
+    i++;
+}
+i=0;
+while(i<n)
+{
+    let j=i+1
+    let p=1;
+     while(j<n)
+     {
+        if(a[i]==a[j])
+        {
+            k=j;
+            p++;
+            while(k<n)
+            {
+                a[k]=a[k+1]
+                k=k+1
+            }
+             n=n-1;
+        }
+        else
+        {
+            j++;
+        }
+     }
+      console.log(a[i]+"-"+p);
+      i++;
+}
