5b9f1f801d80e8340984896121efbdb6c529aa48
PallaviBaghel <163231751+PallaviBaghel@users.noreply.github.com>
2024-03-14 18:32:32 +0530

    pattern
    pattern

---
+++ loop20.js
@@ -0,0 +1,38 @@
+const input = require("readline-sync");
+let n=input.questionInt("enter the number: ");
+let i=1,s="";
+while(i<n)
+{
+    let j=1;
+     while(j<=i)
+     {
+        if(j==1)
+        {
+            s=s+("*")
+            j++;
+        } else if(j==i)
+           {
+              s=s+("*")
+              j++;
+           }  
+           else
+           {
+             s=s+(" ")
+             j++;
+           }
+     }  
+          s=s+("\n")
+          if(i==n)
+          {
+            j++;
+          }
+          else
+          {
+            i++;
+          }
+}  if(i==n)
+{
+    s=s+("*****");
+    console.log(s);
+}
+
